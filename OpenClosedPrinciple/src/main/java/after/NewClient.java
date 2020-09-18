f668c83bc380e6ea25e51cae98a571ff3b96dec8
乌敬林 <773931126@qq.com>
2020-09-18 13:35:56 +0800

    通过使用开闭原则的设计，两者前后的主要区别在于对于子类chart对象的创建时机，对于前者使用switch来说，要在分支中创建对象，而且创建对象和执行方法同时进行，而对于改良后引入抽象类，chartDisplay只需要负责接收创建好的对象，在执行方法即可，增加新的图表时，也只需要添加新的类即可。

--- OpenClosedPrinciple/src/main/java/after/NewClient.java
+++ OpenClosedPrinciple/src/main/java/after/NewClient.java
@@ -12,19 +12,21 @@ public class NewClient {
 
     /**
      *
-     *
      * 通过使用开闭原则的设计，两者前后的主要区别在于对于子类chart对象的创建时机，
      * 对于前者使用switch来说，要在分支中创建对象，而且创建对象和执行方法同时进行，
      * 而对于改良后引入抽象类，chartDisplay只需要负责接
      * 收创建好的对象，在执行方法即可，增加新的图表时，也只需要添加新的类即可。
-     *1
+     *
      */
+
     public static void main(String[] args) {
+
         ChartDisplay chartDisplay = new ChartDisplay();
+
         //此时仅仅需要传入不同的Chart对象即可，也就是修改或添加时，只需要操作实现AbstractChart抽象类的子类即可。
         chartDisplay.setChart(new PieChart());
 
-       // chartDisplay.setChart(new BarChart());
+        // chartDisplay.setChart(new BarChart());
 
         chartDisplay.display();
     }
